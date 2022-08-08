5c11e12421d58cb1da28ef8f48b452dd461932d8
R, Venkateshwaran (mnp3209) <venkateshwaran.r@teliacompany.com>
2022-08-08 14:54:55 +0530

    Testing the AWS pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Vaa Venkat!, Nee Varuvanu enaku theriyum'));
+app.get('/', (req, res) => res.send('Vaa Venkat, Nee Varuvanu enaku theriyum'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
